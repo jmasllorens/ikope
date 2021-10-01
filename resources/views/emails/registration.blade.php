af2bda1d55c7f4c921f962d8af356f77321f7c37
Jael Masllorens <jmasllorens@gmail.com>
2021-10-01 16:38:34 +0200

    Registration mail

---
+++ resources/views/emails/registration.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<body>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div style="width:10rem">
+                <img style="width:30%; margin-top:0.5rem; margin-bottom:0.5rem; border-radius:10rem" src="{{asset('storage/ik.png')}}" alt="ikope logo">
+
+            </div> 
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">            
+                <h4>You have been successfully registered to I-Kope!</h4>
+                <p>We are happy to have you among us, and we hope you enjoy I-Kope. :)</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
