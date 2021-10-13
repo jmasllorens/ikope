6ce2a73a4d2e6545c304ffe61c81e20bd9679cb9
Jael Masllorens <jmasllorens@gmail.com>
2021-10-13 11:54:27 +0200

    Correction of contact form + Total users brought to admin dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -40,11 +40,37 @@
             </div>
             <div v-if="$props.auth.user.isAdmin" class="bg-white rounded-lg p-6">
                 <div class="flex items-center space-x-6 mb-4">
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
+                    <div class="p-3 rounded-full bg-blueish bg-opacity-75">
+                                        <svg class="h-8 w-8 text-white" viewBox="0 0 28 30" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M18.2 9.08889C18.2 11.5373 16.3196 13.5222 14 13.5222C11.6804 13.5222 9.79999 11.5373 9.79999 9.08889C9.79999 6.64043 11.6804 4.65556 14 4.65556C16.3196 4.65556 18.2 6.64043 18.2 9.08889Z"
+                                                fill="currentColor"></path>
+                                            <path
+                                                d="M25.2 12.0444C25.2 13.6768 23.9464 15 22.4 15C20.8536 15 19.6 13.6768 19.6 12.0444C19.6 10.4121 20.8536 9.08889 22.4 9.08889C23.9464 9.08889 25.2 10.4121 25.2 12.0444Z"
+                                                fill="currentColor"></path>
+                                            <path
+                                                d="M19.6 22.3889C19.6 19.1243 17.0927 16.4778 14 16.4778C10.9072 16.4778 8.39999 19.1243 8.39999 22.3889V26.8222H19.6V22.3889Z"
+                                                fill="currentColor"></path>
+                                            <path
+                                                d="M8.39999 12.0444C8.39999 13.6768 7.14639 15 5.59999 15C4.05359 15 2.79999 13.6768 2.79999 12.0444C2.79999 10.4121 4.05359 9.08889 5.59999 9.08889C7.14639 9.08889 8.39999 10.4121 8.39999 12.0444Z"
+                                                fill="currentColor"></path>
+                                            <path
+                                                d="M22.4 26.8222V22.3889C22.4 20.8312 22.0195 19.3671 21.351 18.0949C21.6863 18.0039 22.0378 17.9556 22.4 17.9556C24.7197 17.9556 26.6 19.9404 26.6 22.3889V26.8222H22.4Z"
+                                                fill="currentColor"></path>
+                                            <path
+                                                d="M6.64896 18.0949C5.98058 19.3671 5.59999 20.8312 5.59999 22.3889V26.8222H1.39999V22.3889C1.39999 19.9404 3.2804 17.9556 5.59999 17.9556C5.96219 17.9556 6.31367 18.0039 6.64896 18.0949Z"
+                                                fill="currentColor"></path>
+                                        </svg>
+                                    </div>
                     <div>
                         <p class="text-xl text-gray-700 font-normal mb-1">Users</p>
-                        <p class="text-base text-blue-500 font-normal">I-KOPE management</p>
+                       <div class="text-gray-500">Total: {{users.length}}</div>
                     </div>
+                   
+
+
+               
                 </div>
                 <div>
                     <p class="text-gray-500 leading-loose font-normal text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
@@ -100,7 +126,7 @@ import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue'
 import { Head } from '@inertiajs/inertia-vue3';
 
 export default {
-    props: ['auth'],
+    props: ['auth', 'users'],
 
     components: {
         BreezeAuthenticatedLayout,
